f58ba83241b79b44f56cdae24673b83fe045afc4
Arnold <arnoldclq3@gmail.com>
2020-09-22 13:03:53 -0300

    v1.2 - Se agrega el primer controller de la API y configuracion para deploy en Heroku
    * Se agrega primer controller de la API
    
    * Se modifica Procfile para deploy en heroku
    
    * Se agregan archivos para deploy en heroku
    
    * Update Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: _url-Short-App
--- main.go
+++ main.go
@@ -1,8 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	miNombre := "Arnold"
-	fmt.Printf("Hola mundo! yo soy: %v \n", miNombre)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", Home).Methods("GET")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Printf("Listening...")
+	if err := http.ListenAndServe(":"+port, r); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	welcome := "Hola mundo! yo soy Arnold"
+	respondWithJSON(w, http.StatusOK, welcome)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
 }
