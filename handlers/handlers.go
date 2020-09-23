121ccc1547262a3bb77a758ae4194a5f8f5c3c8a
Arnold <arnoldclq3@gmail.com>
2020-09-23 15:13:48 -0300

    v1.6 - Se crea un pkg de handlers y se mueven todos ahi
    * Se crea un pkg de handlers y se mueven todos ahi
    
    * Se cambia el verbo para eliminar urls

---
+++ handlers/handlers.go
@@ -0,0 +1,140 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/_url-Short-App/business"
+	"github.com/_url-Short-App/services"
+)
+
+type UrlDel struct {
+	Url      string
+	ShortUrl string
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	nombre := "Arnold"
+
+	welcome := "Hola mundo! yo soy " + nombre + " Y esta es mi 1ra app en GO"
+
+	// welcome := struct {
+	// 	MiNombre   string
+	// 	Mensaje    string
+	// 	VersionApp string
+	// }{
+	// 	"Hola mundo! yo soy " + nombre,
+	// 	"Y esta es mi 1ra app en GO",
+	// 	"v1.7",
+	// }
+
+	respondWithJSON(w, http.StatusOK, welcome)
+}
+
+func GenerateShortUrl(w http.ResponseWriter, r *http.Request) {
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "URL no valida")
+	}
+
+	var url string
+	json.Unmarshal(reqBody, &url)
+
+	myShortener := services.NewShortenerBase26()
+	mydb := services.NewMongoService()
+	urlBiz := business.NewUrlBusiness(*myShortener, mydb)
+
+	shortUrl := urlBiz.GenerateShortURL(url)
+
+	result := struct {
+		UrlGen string
+	}{
+		shortUrl,
+	}
+
+	respondWithJSON(w, http.StatusOK, result)
+}
+
+func RestoreUrl(w http.ResponseWriter, r *http.Request) {
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "URL no valida")
+	}
+
+	var url string
+	json.Unmarshal(reqBody, &url)
+
+	myShortener := services.NewShortenerBase26()
+	mydb := services.NewMongoService()
+	urlBiz := business.NewUrlBusiness(*myShortener, mydb)
+
+	originUrl := urlBiz.RestoreOriginalURL(url)
+
+	result := struct {
+		Url string
+	}{
+		originUrl,
+	}
+
+	respondWithJSON(w, http.StatusOK, result)
+}
+
+func DeleteUrl(w http.ResponseWriter, r *http.Request) {
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "URL no valida")
+	}
+
+	var urlDel UrlDel
+	err = json.Unmarshal(reqBody, &urlDel)
+	if err != nil {
+		fmt.Fprintf(w, "Tipo no Valido")
+	}
+
+	myShortener := services.NewShortenerBase26()
+	mydb := services.NewMongoService()
+	urlBiz := business.NewUrlBusiness(*myShortener, mydb)
+	result := "Eliminado"
+	if urlDel.Url != "" {
+		err = urlBiz.DeleteByUrl(urlDel.Url)
+	} else if urlDel.ShortUrl != "" {
+		err = urlBiz.DeleteByShortUrl(urlDel.ShortUrl)
+	} else {
+		result = "url no existe"
+	}
+
+	if err != nil {
+		result = "Error al eliminar"
+	}
+
+	respondWithJSON(w, http.StatusOK, result)
+}
+
+func GetbyId(w http.ResponseWriter, r *http.Request) {
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "URL no valida")
+	}
+
+	var id int
+	json.Unmarshal(reqBody, &id)
+
+	myShortener := services.NewShortenerBase26()
+	mydb := services.NewMongoService()
+	// mydb := services.NewMockDataBase()
+	urlBiz := business.NewUrlBusiness(*myShortener, mydb)
+
+	result, err := urlBiz.GetbyId(id)
+	_ = err
+
+	respondWithJSON(w, http.StatusOK, result)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
--- main.go
+++ main.go
@@ -1,24 +1,23 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/_url-Short-App/business"
-	"github.com/_url-Short-App/services"
+	"github.com/_url-Short-App/handlers"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", Home).Methods("GET")
-	r.HandleFunc("/GenerateShortUrl", GenerateShortUrl).Methods("GET")
-	r.HandleFunc("/Get", GetbyId).Methods("GET")
+	r.HandleFunc("/", handlers.Home).Methods("GET")
+	r.HandleFunc("/GenerateShortUrl", handlers.GenerateShortUrl).Methods("POST")
+	r.HandleFunc("/RestoreUrl", handlers.RestoreUrl).Methods("GET")
+	r.HandleFunc("/DeleteUrl", handlers.DeleteUrl).Methods("DELETE")
+
+	r.HandleFunc("/Get", handlers.GetbyId).Methods("GET")
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -29,55 +28,4 @@ func main() {
 	if err := http.ListenAndServe(":"+port, r); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	welcome := "Hola mundo! yo soy Arnold"
-	respondWithJSON(w, http.StatusOK, welcome)
-}
-
-func GenerateShortUrl(w http.ResponseWriter, r *http.Request) {
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "URL no valida")
-	}
-
-	var url string
-	json.Unmarshal(reqBody, &url)
-
-	myShortener := services.NewShortenerBase26()
-	mydb := services.NewMongoService()
-	// mydb := services.NewMockDataBase()
-	urlBiz := business.NewUrlBusiness(*myShortener, mydb)
-
-	shorUrl := urlBiz.GenerateShortURL(url)
-
-	result := "La url generada es: " + string(shorUrl)
-	respondWithJSON(w, http.StatusOK, result)
-}
-
-func GetbyId(w http.ResponseWriter, r *http.Request) {
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "URL no valida")
-	}
-
-	var id int
-	json.Unmarshal(reqBody, &id)
-
-	myShortener := services.NewShortenerBase26()
-	mydb := services.NewMongoService()
-	// mydb := services.NewMockDataBase()
-	urlBiz := business.NewUrlBusiness(*myShortener, mydb)
-
-	result, err := urlBiz.GetbyId(id)
-	_ = err
-
-	respondWithJSON(w, http.StatusOK, result)
-}
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(response)
 }
